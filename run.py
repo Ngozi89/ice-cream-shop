322acfa918eab839260902134d436443df71031f
Ngozi Omenka <ngozi.madu@live.se>
2024-07-20 11:05:21 +0000

    Added welcome message and allow user chose if the want o enter data or not

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ice_cream_shop')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
@@ -60,6 +61,29 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully.\n")
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def delete_sales_worksheet(data):
+    print("Deleting data from worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet deleted successfully.\n")
+
+
+
+def main():
+    """
+    For all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    delete_sales_worksheet(sales_data)
+
+
+print("Welcome to Ice-craem data automation")
+# Let user choose if they want to see sales data or quit
+sales_data = input("Do you want to update the sales data? ")
+print()
+# Check If user condition is true. If yes then let user enter data else quit
+if sales_data.lower() != "yes":
+    quit()
+main()
